502e33e6c036175dec7db8970c6fdea6dee99f8c
DayanaDGam <ddavila1109@gmail.com>
2025-03-27 08:36:56 -0500

    javascript

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const popup = document.querySelector(".popup");
-    const editButton = document.querySelector(".main_button_edit");
-    const closeButton = document.querySelector(".popup_button_close");
+    const editButton = document.querySelector(".main__button_edit");
+    const closeButton = document.querySelector(".popup__button_close");
 
+    
     editButton.addEventListener("click", function () {
         popup.style.display = "block";
     });
 
+
     closeButton.addEventListener("click", function () {
         popup.style.display = "none";
-    }); 
+    });
 });
+
