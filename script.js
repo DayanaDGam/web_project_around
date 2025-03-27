a2a85b5f5aa8925b23b122ff5d65d091eefed116
DayanaDGam <ddavila1109@gmail.com>
2025-03-27 09:00:04 -0500

    medias querys

--- script.js
+++ script.js
@@ -4,12 +4,31 @@ document.addEventListener("DOMContentLoaded", function () {
     const closeButton = document.querySelector(".popup__button_close");
 
     
+    const profileName = document.querySelector(".main__paragraph_name");
+    const profileAbout = document.querySelector(".main__paragraph_about");
+
+    
+    const inputName = document.querySelector(".popup__input_name");
+    const inputAbout = document.querySelector(".popup__input_about");
+
+    
     editButton.addEventListener("click", function () {
-        popup.style.display = "block";
+        inputName.value = profileName.textContent; 
+        inputAbout.value = profileAbout.textContent; 
+        popup.style.display = "block"; 
     });
 
-
     closeButton.addEventListener("click", function () {
+        popup.style.display = "none";
+    });
+
+    const saveButton = document.querySelector(".popup__button_save");
+    saveButton.addEventListener("click", function (event) {
+        event.preventDefault(); 
+
+        profileName.textContent = inputName.value; 
+        profileAbout.textContent = inputAbout.value;
+
         popup.style.display = "none";
     });
 });
