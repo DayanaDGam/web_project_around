39132e906469c9c58b75e13b8e1da5852925b67c
DayanaDGam <ddavila1109@gmail.com>
2025-03-28 10:34:12 -0500

    script

---
+++ scripts/index.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const popup = document.querySelector(".popup");
+    const editButton = document.querySelector(".main__button_edit");
+    const closeButton = document.querySelector(".popup__button_close");
+
+    
+    const profileName = document.querySelector(".main__paragraph_name");
+    const profileAbout = document.querySelector(".main__paragraph_about");
+
+    
+    const inputName = document.querySelector(".popup__input_name");
+    const inputAbout = document.querySelector(".popup__input_about");
+
+    
+    editButton.addEventListener("click", function () {
+        inputName.value = profileName.textContent; 
+        inputAbout.value = profileAbout.textContent; 
+        popup.style.display = "block"; 
+    });
+
+    closeButton.addEventListener("click", function () {
+        popup.style.display = "none";
+    });
+
+    const saveButton = document.querySelector(".popup__button_save");
+    saveButton.addEventListener("click", function (event) {
+        event.preventDefault(); 
+
+        profileName.textContent = inputName.value; 
+        profileAbout.textContent = inputAbout.value;
+
+        popup.style.display = "none";
+    });
+});
+
+    const likeButtons = document.querySelectorAll(".main__button_like");
+  
+    likeButtons.forEach(button => {
+      button.addEventListener("click", function () {
+        if (this.classList.contains("main__button_like_active")) {
+          this.classList.remove("main__button_like_active");
+          this.style.backgroundImage = "url(../images/like-white.svg)"; 
+        } else {
+          this.classList.add("main__button_like_active");
+          this.style.backgroundImage = "url(../images/like-active.jpeg)"; 
+        }
+      });
+    });
+  
+  
+
