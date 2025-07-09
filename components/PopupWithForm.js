535bdc84987e4fbddf939f73931dc2452ceca0fd
DayanaDGam <ddavila1109@gmail.com>
2025-07-09 12:44:19 -0500

    refactorizacion clases

---
+++ components/PopupWithForm.js
@@ -0,0 +1,38 @@
+import { Popup } from './Popup.js';
+
+export class PopupWithForm extends Popup {
+  constructor(popupSelector, handleFormSubmit) {
+    super(popupSelector);
+    this._handleFormSubmit = handleFormSubmit;
+    this._form = this._popup.querySelector('form');
+    this._inputList = Array.from(this._form.querySelectorAll('input'));
+  }
+
+  _getInputValues() {
+    const formValues = {};
+    this._inputList.forEach(input => {
+      formValues[input.name] = input.value;
+    });
+    return formValues;
+  }
+
+  setInputValues(data) {
+    this._inputList.forEach((input) => {
+      input.value = data[input.name];
+    });
+  }
+
+  setEventListeners() {
+    super.setEventListeners();
+    this._form.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+      this._handleFormSubmit(this._getInputValues());
+      this.close();
+    });
+  }
+
+  close() {
+    super.close();
+    this._form.reset();
+  }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,133 +1,66 @@
-import { Card } from './Card.js';
-import { FormValidator } from './FormValidator.js';
-import { validationConfig } from './utils.js';
+import { Card } from '../components/Card.js';
+import { Section } from '../components/Section.js';
+import { FormValidator } from '../components/FormValidator.js';
+import { PopupWithForm } from '../components/PopupWithForm.js';
+import { PopupWithImage } from '../components/PopupWithImage.js';
+import { UserInfo } from '../components/UserInfo.js';
+import { validationConfig, initialCards } from '../constants/utils.js';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const initialCards = [
-    { name: 'Valle de Yosemite', link: './images/valle_yosemite.png' },
-    { name: 'Lago Louise', link: './images/lago_louise.png' },
-    { name: 'Montañas Calvas', link: './images/montanas_calvas.png' },
-    { name: 'Latemar', link: './images/latemar.png' },
-    { name: 'Vanois National Park', link: './images/vanois_national_park.png' },
-    { name: 'Lago Di Braies', link: './images/lago_di_braies.png' }
-  ];
+  const userInfo = new UserInfo({
+    nameSelector: '.main__paragraph_name',
+    aboutSelector: '.main__paragraph_about'
+  });
 
-  const cardList = document.querySelector('.main__gallery');
-  const formElement = document.querySelector('#formAdd');
-  const nameInput = formElement.querySelector('#title-input');
-  const linkInput = formElement.querySelector('#url-input');
-  const addCardButton = document.querySelector('.main__button_add');
-  const popup = document.querySelector('#popupAdd');
-  const closePopupButton = popup.querySelector('.popup__button_close');
-
-  const editProfileButton = document.querySelector('.main__button_edit');
-  const popupEdit = document.querySelector('#popupEdit');
-  const closeEditPopupButton = popupEdit.querySelector('.popup__button_close');
-  const formEditElement = document.querySelector('#formEdit');
-
-  const imagePopup = document.querySelector('.popup_image');
-  const imagePopupImg = imagePopup.querySelector('.popup__image');
-  const imagePopupCaption = imagePopup.querySelector('.popup__caption');
-  const closeImagePopupButton = imagePopup.querySelector('.popup__button_close-image');
-
-  function handleCardClick(name, link) {
-    imagePopupImg.src = link;
-    imagePopupImg.alt = name;
-    imagePopupCaption.textContent = name;
-    openPopup(imagePopup);
-  }
+  const imagePopup = new PopupWithImage('.popup_image');
+  imagePopup.setEventListeners();
 
   function createCard(data) {
-    const card = new Card(data, '#main__template', handleCardClick);
+    const card = new Card(data, '#main__template', (name, link) => {
+      imagePopup.open(name, link);
+    });
     return card.generateCard();
   }
 
-  function renderCard(data) {
-    const cardElement = createCard(data);
-    cardList.prepend(cardElement);
-  }
-
-  function openPopup(popupElement) {
-  popupElement.classList.add('popup_opened');
-  document.addEventListener('keydown', handleEscClose);
-}
-
-  function closePopup(popupElement) {
-  popupElement.classList.remove('popup_opened');
-  document.removeEventListener('keydown', handleEscClose);
-}
-
-  function handleEscClose(evt) {
-  if (evt.key === 'Escape') {
-    const openedPopup = document.querySelector('.popup_opened');
-    if (openedPopup) {
-      closePopup(openedPopup);
+  const section = new Section({
+    items: initialCards,
+    renderer: (cardData) => {
+      const cardElement = createCard(cardData);
+      section.addItem(cardElement);
     }
-  }
-}
+  }, '.main__gallery');
+  section.renderItems();
 
-  function handleFormSubmit(evt) {
-    evt.preventDefault();
-    const newCardData = {
-      name: nameInput.value,
-      link: linkInput.value
-    };
-    renderCard(newCardData);
-    formElement.reset();
-    formValidatorAdd.resetValidation();
-    closePopup(popup);
-  }
-
-  if (formElement) {
-    formElement.addEventListener('submit', handleFormSubmit);
-  }
-
-  if (addCardButton) {
-    addCardButton.addEventListener('click', () => openPopup(popup));
-  }
-
-  if (closePopupButton) {
-    closePopupButton.addEventListener('click', () => closePopup(popup));
-  }
-
-  if (editProfileButton) {
-    editProfileButton.addEventListener('click', () => openPopup(popupEdit));
-  }
-
-  if (closeEditPopupButton) {
-    closeEditPopupButton.addEventListener('click', () => closePopup(popupEdit));
-  }
-
-  if (formEditElement) {
-    formEditElement.addEventListener('submit', (evt) => {
-      evt.preventDefault();
-      const name = formEditElement.querySelector('#name-input').value;
-      const about = formEditElement.querySelector('#about-input').value;
-
-      document.querySelector('.main__paragraph_name').textContent = name;
-      document.querySelector('.main__paragraph_about').textContent = about;
-
-      closePopup(popupEdit);
-    });
-  }
-
-  if (closeImagePopupButton) {
-    closeImagePopupButton.addEventListener('click', () => closePopup(imagePopup));
-  }
-
-  const formValidatorAdd = new FormValidator(validationConfig, formElement);
-  const formValidatorEdit = new FormValidator(validationConfig, formEditElement);
-
-  formValidatorAdd.enableValidation();
-  formValidatorEdit.enableValidation();
-
-  initialCards.forEach(renderCard);
-
-  document.querySelectorAll('.popup').forEach((popup) => {
-  popup.addEventListener('mousedown', (evt) => {
-    if (evt.target === popup) {
-      closePopup(popup);
-    }
+  const popupEditProfile = new PopupWithForm('#popupEdit', (formData) => {
+    userInfo.setUserInfo(formData);
+    popupEditProfile.close();
   });
-});
+  popupEditProfile.setEventListeners();
+
+  document.querySelector('.main__button_edit').addEventListener('click', () => {
+    const currentUser = userInfo.getUserInfo();
+    popupEditProfile.setInputValues(currentUser);
+    popupEditProfile.open();
+  });
+
+  const popupAddCard = new PopupWithForm('#popupAdd', (formData) => {
+    const newCard = createCard({ name: formData['title-input'], link: formData['url-input'] });
+    section.addItem(newCard);
+    popupAddCard.close();
+  });
+  popupAddCard.setEventListeners();
+
+
+  document.querySelector('.main__button_add').addEventListener('click', () => {
+    popupAddCard.open();
+  });
+
+
+  const formValidators = {};
+  document.querySelectorAll('form').forEach((formElement) => {
+    const validator = new FormValidator(validationConfig, formElement);
+    const formName = formElement.getAttribute('id');
+    formValidators[formName] = validator;
+    validator.enableValidation();
+  });
 });
