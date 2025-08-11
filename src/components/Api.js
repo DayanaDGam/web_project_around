4a158d6d414dcba5063f1e10cd643f1994ce2bd2
DayanaDGam <ddavila1109@gmail.com>
2025-08-11 16:58:25 -0500

    API

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,5 +1,5 @@
 function fetchUser() {
-    fetch("https://around-api.es.tripleten-services.com/v1//users/me", {
+    fetch("https://around-api.es.tripleten-services.com/v1/users/me", {
   headers: {
     authorization: "81ee6d99-9b3c-40e0-aeb8-7f9ed65e18da"
   }
